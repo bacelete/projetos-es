d48a4b0bca696355d808a99ffd587d8ba88da1fe
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-02-23 12:39:19 -0300

    Refatoração do código + gerarMensagemUsuario() e reiniciarJogo()

--- exercicios-javascript/exercicio-adivinhe-o-numero-js/assets/script.js
+++ exercicios-javascript/exercicio-adivinhe-o-numero-js/assets/script.js
@@ -1,13 +1,15 @@
-const minValue = 1;
-const maxValue = 100; 
+const MIN_VALUE = 1;
+const MAX_VALUE = 100; 
+const QTD_MAX_TURNO = 10; 
 const divTentativas = document.getElementById("tentativas");
 const campoPalpite = document.getElementById('inp-texto');
 const btnEnviarPalpite = document.getElementById('btnEnviar');
+const divReiniciarJogo = document.getElementById('restart-game'); 
 let turno = 1;
 let tentativas = [];
 
 function gerarNumeroAleatorio() {
-    return Math.floor(Math.random() * (maxValue - minValue) + minValue);
+    return Math.floor(Math.random() * (MAX_VALUE - MIN_VALUE) + MIN_VALUE);
 }
 
 let valorGerado = gerarNumeroAleatorio();
@@ -31,12 +33,28 @@ function validarEntradaUsuario() {
 function verificarTentativa(palpite) {
     if (palpite === valorGerado) {
         alert("Parabéns, você acertou!"); 
-        bloquearEntradaUsuario(); 
+        btnEnviarPalpite.disabled = "true"; 
+        gerarMensagemUsuario(); 
     } 
+    if ()
 }
 
-function bloquearEntradaUsuario() {
-    btnEnviarPalpite.disabled = "true"; 
+function gerarMensagemUsuario() {
+    let elemReiniciar = document.createElement("span");
+    elemReiniciar.innerHTML = "Reiniciar o jogo?"
+
+    let btnReiniciar = document.createElement("button");
+    btnReiniciar.id = "btnReiniciar"; 
+    btnReiniciar.innerHTML = "Sim"; 
+
+    divReiniciarJogo.appendChild(elemReiniciar);
+    divReiniciarJogo.appendChild(btnReiniciar); 
+
+    btnReiniciar.addEventListener("click", reiniciarJogo);
+}
+
+function reiniciarJogo() {
+    window.location.reload(true); 
 }
 
 function imprimirTentativa() {
