bffa9b178ddee564365ddc1991083dc42ddcc2ad
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-02-23 21:23:53 -0300

    Alteração HTML + função de click JS

--- exercicios-javascript/projeto-calculadora-js/assets/app.js
+++ exercicios-javascript/projeto-calculadora-js/assets/app.js
@@ -1,6 +1,15 @@
 const btnNumbers = document.getElementsByClassName('btn-number');
 const operators = document.getElementsByClassName('operators'); 
+const display = document.getElementsByClassName('display');
 
 function imprimirNaTela() {
+    
+}
 
+for (btn of btnNumbers) {
+    btn.addEventListener("click", imprimirNaTela)
+}
+
+for (op of operators) {
+    op.addEventListener("click", imprimirNaTela);
 }
