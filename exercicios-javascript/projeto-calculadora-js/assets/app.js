8c267710deb67cf3e062fea626e6e1a513d69aac
Arthur Bacelete <abacelete@sga.pucminas.br>
2025-02-25 12:14:33 -0300

    Função validarNumero()
    Foi criado uma função de validar número para evitar divisão por 0. A função está incompleta, mas funcional.

--- exercicios-javascript/projeto-calculadora-js/assets/app.js
+++ exercicios-javascript/projeto-calculadora-js/assets/app.js
@@ -1,21 +1,21 @@
-const operators = document.getElementsByClassName('operators'); 
+const operators = document.getElementsByClassName('operators');
 const display = document.querySelector('.display');
 const buttons = document.getElementsByClassName('btn');
 
-let arrValoresDisplay = []; 
+let arrValoresDisplay = [];
 
 function clicarBotoesCalculadora(event) {
     let btnValor = event.target.textContent;
 
     if (btnValor === "C" || btnValor === "CE") {
-        limparDisplay(); 
+        limparDisplay();
         return;
     }
     if (btnValor === "=") {
-        realizarOperacoes(); 
+        realizarOperacoes();
         return;
     }
-    if(btnValor === "±") {
+    if (btnValor === "±") {
         inverterSinal();
         return;
     }
@@ -37,7 +37,7 @@ function atualizarDisplay() {
 
 function removerCaracter() {
     arrValoresDisplay.pop();
-    atualizarDisplay(); 
+    atualizarDisplay();
 }
 
 /*
@@ -52,26 +52,39 @@ function transformarEmPontoFlutuante() {
 }
 */
 
+function validarNumero() {
+    let i = arrValoresDisplay.indexOf('/');
+    let aux = arrValoresDisplay.indexOf('0');
+
+    if (aux = i + 1) {
+        display.innerHTML = "Error";
+        return false;
+    }
+    return true;
+}
+
 function inverterSinal() {
     display.innerHTML = (-1) * display.innerHTML;
     arrValoresDisplay.pop();
     arrValoresDisplay.push(display.innerHTML);
 }
 
-function limparDisplay() { 
+function limparDisplay() {
     arrValoresDisplay = [];
     display.innerHTML = 0;
 }
 
 function realizarOperacoes() {
-    let resultadoExpressao = eval(arrValoresDisplay.join(''));
+    if (validarNumero()) {
+        let resultadoExpressao = eval(arrValoresDisplay.join(''));
 
-    while(arrValoresDisplay.length) {
-        arrValoresDisplay.pop();
+        while (arrValoresDisplay.length) {
+            arrValoresDisplay.pop();
+        }
+
+        arrValoresDisplay.push(resultadoExpressao);
+        display.innerHTML = resultadoExpressao;
     }
-    
-    arrValoresDisplay.push(resultadoExpressao);
-    display.innerHTML = resultadoExpressao;
 }
 
 for (let btn of buttons) {
