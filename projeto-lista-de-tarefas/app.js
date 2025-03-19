03698b3a1e97ca771785131f60e596c885472e12
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-03-19 10:47:59 -0300

    new: ejs module + views + express.static

--- projeto-lista-de-tarefas/app.js
+++ projeto-lista-de-tarefas/app.js
@@ -2,20 +2,22 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const app = express(); 
-const PORT = 3000; 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use('/static', express.static(path.join(__dirname, 'view')))
+const app = express(); 
+const PORT = 3000; 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'view', 'html'));
+app.use('/static', express.static(path.join(__dirname, 'src', 'view', 'css')));
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em: http://localhost:${PORT}`);
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!'); 
+    res.render('login');
 })
 
 export default app; 
