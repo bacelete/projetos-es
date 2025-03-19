5f5fc5d4f954172302b51788d912aa5f59e0cb8a
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-03-19 11:22:01 -0300

    update: add comments

--- projeto-lista-de-tarefas/app.js
+++ projeto-lista-de-tarefas/app.js
@@ -9,14 +9,17 @@ const __dirname = path.dirname(__filename);
 const app = express(); 
 const PORT = 3000; 
 
+// view and static files conf:
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'view', 'html'));
 app.use('/static', express.static(path.join(__dirname, 'src', 'view', 'css')));
 
+// create a server:
 app.listen(PORT, () => {
     console.log(`Servidor rodando em: http://localhost:${PORT}`);
 });
 
+//use routes from 'routes.js'
 app.use('/', routes); 
 
 export default app; 
