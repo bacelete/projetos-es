d8727e29cb2ae655bee1f7e4bdb6a1a7e73472aa
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-03-19 19:46:56 -0300

    update: async delete(id)

--- projeto-lista-de-tarefas/src/model/UsuarioRepository.js
+++ projeto-lista-de-tarefas/src/model/UsuarioRepository.js
@@ -2,11 +2,11 @@ import conexao from '../database/connection.js'
 
 class UsuarioRepository {
     static findById(id) {
-        const sql = "SELECT * FROM usuario WHERE id = ?"; 
+        const sql = "SELECT * FROM usuario WHERE id = ?";
 
         return new Promise((resolve, reject) => {
             conexao.query(sql, [id], (err, result) => {
-                if (err) throw reject(err); 
+                if (err) throw reject(err);
                 console.log(result[0]);
                 return resolve(result[0]);
             });
@@ -16,7 +16,7 @@ class UsuarioRepository {
     static create(usuario) {
         const username = usuario.username;
         const password = usuario.password;
-        const sql = "INSERT INTO usuario(username, password) VALUES (?, ?)"; 
+        const sql = "INSERT INTO usuario(username, password) VALUES (?, ?)";
 
         conexao.query(sql, [username, password], (err, result) => {
             if (err) throw err;
@@ -24,13 +24,20 @@ class UsuarioRepository {
         })
     }
 
-    static delete(id) {
+    static async delete(id) {
         const sql = "DELETE FROM usuario WHERE id = ?"
-        if (this.findById(id) !== null) {
-            conexao.query(sql, [id], (err, result) => {
-                if (err) throw err;
-                return JSON.parse(JSON.stringify(result));
+
+        const user = await this.findById(id);
+        console.log(user);
+
+        if (user !== null && user !== undefined) {
+            return new Promise((resolve, reject) => {
+                conexao.query(sql, [id], (err, result) => {
+                    if (err) throw reject(err);
+                    return JSON.parse(JSON.stringify(resolve(result)));
+                })
             })
+
         }
     }
 }
