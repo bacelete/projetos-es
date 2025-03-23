41e1ba5c6729a359349a70911a19c07e57eccae9
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-03-23 20:34:17 -0300

    new: refatoração html + js

--- projeto-lista-de-tarefas/src/view/js/script.js
+++ projeto-lista-de-tarefas/src/view/js/script.js
@@ -2,6 +2,8 @@ const input = document.getElementById('task-input');
 const addTask = document.getElementById('addTask');
 const taskList = document.getElementById('task-list');
 
+const port = process.env.port || "8000"; 
+
 function adicionarTarefa() {
     let strInput = input.value;
     if (strInput !== "" && typeof(strInput) === 'string') {
@@ -14,7 +16,15 @@ function gerarTarefa(input) {
     task.className = 'task'; 
     task.innerHTML = input;
     
+    salvarTarefa(task);
     taskList.appendChild(task);
+}
+
+function salvarTarefa(tarefa) {
+    const request = new Request(`http://localhost:${port}/task`, {
+        method: 'POST',
+        body: {nome: tarefa.nome, status: tarefa.status}
+    });
 }
 
 addTask.addEventListener('click', adicionarTarefa);
