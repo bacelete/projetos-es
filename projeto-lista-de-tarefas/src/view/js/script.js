e2e22ebd92145f71d5c7d390ec3b007bebf7999e
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-03-23 21:05:11 -0300

    new: method post ok

--- projeto-lista-de-tarefas/src/view/js/script.js
+++ projeto-lista-de-tarefas/src/view/js/script.js
@@ -1,30 +1,55 @@
 const input = document.getElementById('task-input'); 
 const addTask = document.getElementById('addTask');
 const taskList = document.getElementById('task-list');
+const options = document.querySelector('#status-select');
 
-const port = process.env.port || "8000"; 
+const port = 8000;
 
 function adicionarTarefa() {
     let strInput = input.value;
-    if (strInput !== "" && typeof(strInput) === 'string') {
-        gerarTarefa(strInput);
+
+    if (strInput !== "" && typeof(strInput) === 'string' && options.value) {
+        gerarTarefa(strInput, options.value);
     }
 }
 
-function gerarTarefa(input) {
+function gerarTarefa(input, option) {
     let task = document.createElement('li');
     task.className = 'task'; 
     task.innerHTML = input;
+
+    let obj = {nome: input, status: option};
     
-    salvarTarefa(task);
+    salvarTarefa(obj);
     taskList.appendChild(task);
 }
 
 function salvarTarefa(tarefa) {
-    const request = new Request(`http://localhost:${port}/task`, {
-        method: 'POST',
-        body: {nome: tarefa.nome, status: tarefa.status}
-    });
+    try {
+        const request = new Request(`http://localhost:${port}/task`, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({nome: tarefa.nome, status: tarefa.status})
+        });
+        fetch(request)
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json(); 
+                }
+                else {
+                    throw new Error('Houve um erro durante a criação da tarefa');
+                }
+            })
+            .then((data) => {
+                console.log('Tarefa criada com sucesso: '+JSON.stringify(data)); 
+            })
+    }
+    catch(error) {
+        console.log(error); 
+    }
 }
 
 addTask.addEventListener('click', adicionarTarefa);
