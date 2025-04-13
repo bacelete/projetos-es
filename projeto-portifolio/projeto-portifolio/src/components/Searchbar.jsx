a53a2ea8374ff55bc29ea9809ec23e52d18ba580
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-13 20:16:25 -0300

    alteracao tamanho da searchbar

--- projeto-portifolio/projeto-portifolio/src/components/Searchbar.jsx
+++ projeto-portifolio/projeto-portifolio/src/components/Searchbar.jsx
@@ -2,7 +2,7 @@
 function Searchbar() {
     return (
         <div>
-            <input className="bg-white p-1 rounded-sm" type="text" name="searchbar" id="searchbar" placeholder="Pesquisar" />
+            <input className="bg-white p-1 w-2xl rounded-sm" type="text" name="searchbar" id="searchbar" placeholder="Pesquisar" />
         </div>
     )
 }
