c54de75d23eb55bed7d84a2243983fb66c1c80ec
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-13 19:16:07 -0300

    responsividade navbar

--- projeto-portifolio/projeto-portifolio/src/components/Navbar.jsx
+++ projeto-portifolio/projeto-portifolio/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Searchbar from "./Searchbar";
 
 function Navbar() {
     return (
-        <div className="flex bg-[#031626] h-[80px] p-4 gap-5 justify-between items-center">
+        <div className="flex bg-[#031626] h-[80px] p-4 gap-5 justify-between items-center md:flex-auto">
             <h1 className="text-white text-3xl">Shop</h1>
             <Searchbar />
         </div>
