ac4088d86afde362838c325d1e9db3cfbba59990
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-13 20:15:11 -0300

    new: fontawesome componente + import icone do carrinho

--- projeto-portifolio/projeto-portifolio/src/components/Navbar.jsx
+++ projeto-portifolio/projeto-portifolio/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import Searchbar from "./Searchbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
     return (
         <div className="flex bg-[#031626] h-[80px] p-4 gap-5 justify-between items-center md:flex-auto">
             <h1 className="text-white text-3xl">Shop</h1>
             <Searchbar />
+            <FontAwesomeIcon className="w-[30px] text-white" icon={faCartShopping} />
         </div>
     )
 }
--- projeto-portifolio/projeto-portifolio/src/components/Searchbar.jsx
+++ projeto-portifolio/projeto-portifolio/src/components/Searchbar.jsx
@@ -1,3 +1,4 @@
+
 function Searchbar() {
     return (
         <div>
