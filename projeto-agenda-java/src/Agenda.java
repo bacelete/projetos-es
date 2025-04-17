7416d94dc720484664044800a57d6d0519b063af
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-17 16:26:22 -0300

    criacao dos metodos da agenda

--- projeto-agenda-java/src/Agenda.java
+++ projeto-agenda-java/src/Agenda.java
@@ -2,4 +2,21 @@ import java.util.ArrayList;
 
 public class Agenda {
     private ArrayList<Contato> contatos;
+
+    public void adicionarContato(Contato contato) {
+
+    }
+
+    public void listarContatos() {
+        
+    }
+
+    public Contato buscarContato(String nome) { 
+      
+    }
+
+    public void removerContato(String nome) {
+
+    }
+
 }
--- projeto-agenda-java/src/Contato.java
+++ projeto-agenda-java/src/Contato.java
@@ -2,4 +2,7 @@ public class Contato {
     public String nome; 
     public String telefone; 
     public String email;
+
+ 
+
 }
