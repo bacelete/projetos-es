1e733614375ae287ddc6c73327bceae5fb2afdf5
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-17 21:25:50 -0300

    logica inicial do BufferedWriter

--- projeto-agenda-java/src/WriterAndReader.java
+++ projeto-agenda-java/src/WriterAndReader.java
@@ -1,3 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+
 public class WriterAndReader {
-    
+    public static void escrever(String path) throws IOException {
+        try (BufferedWriter buffer = new BufferedWriter(new FileWriter(path))) {
+
+            
+            buffer.close();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
 }
