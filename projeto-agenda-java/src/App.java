e7b126c077b50dd8bee6d142027d81e580fdbbb7
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-17 22:05:04 -0300

    logica do menu funcionando + limpeza do buffer a cada iteracao

--- projeto-agenda-java/src/App.java
+++ projeto-agenda-java/src/App.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    public static Scanner s = new Scanner(System.in); 
+    public static Scanner s = new Scanner(System.in);
+    public static Agenda agenda = new Agenda();
 
     public static void menu() {
         int opcao = -1; 
+        String nome, telefone, email; 
 
         while (opcao != 0) {
             System.out.println("\nSistema de Agenda:");
@@ -16,10 +18,38 @@ public class App {
             System.out.println("0 - Sair do menu");
             System.out.println("Digite a opção que deseja: ");
             opcao = s.nextInt(); 
+            s.nextLine(); 
+
+            switch (opcao) {
+                case 1: 
+                    System.out.println("Nome: ");
+                    nome = s.nextLine();
+                    System.out.println("Telefone: ");
+                    telefone = s.nextLine(); 
+                    System.out.println("E-mail: ");
+                    email = s.nextLine(); 
+
+                    Contato contato = new Contato(nome, telefone, email); 
+                    agenda.adicionarContato(contato);
+                    break; 
+                case 2: 
+                    System.out.println("Nome: ");
+                    nome = s.nextLine(); 
+
+                    agenda.buscarContato(nome); 
+                    break; 
+                case 3: 
+                    System.out.println("Nome: ");
+                    nome = s.nextLine(); 
+                    
+                    agenda.removerContato(nome);
+                    break; 
+                case 4: agenda.listarContatos(); break; 
+            }
         }
     }
 
     public static void main(String[] args) {
-        menu();  
+        menu();
     }
 }
