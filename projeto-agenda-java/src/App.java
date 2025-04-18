e546d5b5a035d432c86232bc52359ee9c681c8b4
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-17 21:52:47 -0300

    obj Scanner + menu funcionando

--- projeto-agenda-java/src/App.java
+++ projeto-agenda-java/src/App.java
@@ -1,8 +1,25 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
+    public static Scanner s = new Scanner(System.in); 
+
+    public static void menu() {
+        int opcao = -1; 
+
+        while (opcao != 0) {
+            System.out.println("\nSistema de Agenda:");
+            System.out.println("1. Adicionar contato");
+            System.out.println("2. Buscar contato pelo nome");
+            System.out.println("3. Remover contato");
+            System.out.println("4. Listar todos os contatos");
+            System.out.println("0 - Sair do menu");
+            System.out.println("Digite a opção que deseja: ");
+            opcao = s.nextInt(); 
+        }
+    }
+
     public static void main(String[] args) {
-        Contato contato = new Contato("Arthur", "31992412598", "arthurbt2005@gmail.com"); 
-        
+        menu();  
     }
 }
