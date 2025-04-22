af870846c4bfa6b2a06f9f03cadb32aa30f6da75
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-22 18:37:53 -0300

    adicionei o mysql-connector-j-9.3.0 e alterei o conteudo da variavel url

--- projeto-agenda-java/src/database/Conexao.java
+++ projeto-agenda-java/src/database/Conexao.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException; 
 
 public class Conexao {
-    private static String url = "jdbc: mysql:/localhost:3000/projeto_agenda"; 
+    private static String url = "jdbc:mysql://localhost:3306/projeto_agenda"; 
     private static String password = "root"; 
     private static String username = "root";  
 
@@ -16,7 +16,7 @@ public class Conexao {
         try {
             if (connection == null) {
                 connection = DriverManager.getConnection(url, username, password); //retorna uma conexao com o banco;
-                System.out.println("Conectado com o banco de dados"+url);
+                System.out.println("Conectado com o banco de dados "+url);
             }
         }  
         catch (SQLException e) {
