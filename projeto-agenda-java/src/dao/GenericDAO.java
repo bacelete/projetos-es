aa64cd94f0aefcf9fd1f5875866e334e09e5e4ad
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-23 12:01:57 -0300

    salvar no banco funcionando com sucesso

--- projeto-agenda-java/src/dao/GenericDAO.java
+++ projeto-agenda-java/src/dao/GenericDAO.java
@@ -34,7 +34,7 @@ public abstract class GenericDAO {
             query.setString(2, email);
             query.setString(3, telefone);
 
-            query.executeUpdate(sql);
+            query.executeUpdate();
 
             System.out.println("Dados salvos com sucesso!");
         } catch (SQLException e) {
