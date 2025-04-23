c851777bbdee52c5c935adccaffd4fc62d73f3e6
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-23 10:00:58 -0300

    construtor da AgendaDAO

--- projeto-agenda-java/src/dao/AgendaDAO.java
+++ projeto-agenda-java/src/dao/AgendaDAO.java
@@ -1,6 +1,8 @@
 package dao;
 
 public class AgendaDAO extends GenericDAO {
+    public AgendaDAO() {}
+
     public void buscarContato(String nome) {
         String sql = "SELECT * WHERE nome = ?"; 
         buscar(sql); 
--- projeto-agenda-java/src/service/AgendaService.java
+++ projeto-agenda-java/src/service/AgendaService.java
@@ -3,5 +3,5 @@ package service;
 import dao.AgendaDAO;
 
 public class AgendaService {
-    
+    AgendaDAO agenda; 
 }
