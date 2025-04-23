0f46a34cae3f26c3c2ca9527fc12b2a05ead1c34
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-22 22:19:26 -0300

    metodo de buscar contato no ContatoDAO.java

--- projeto-agenda-java/src/dao/ContatoDAO.java
+++ projeto-agenda-java/src/dao/ContatoDAO.java
@@ -1,11 +1,26 @@
 package dao;
 
 import java.sql.*; 
+import database.Conexao; //importa a classe do database
 
 public class ContatoDAO {
+    Connection con = Conexao.connect(); 
+
     public void buscarContato(String nome) {
         String sql = "SELECT * WHERE nome = ?"; 
+        try {
+            PreparedStatement query = con.prepareStatement(sql);
+            ResultSet rs = query.executeQuery(sql);
 
-        
+            String nome = rs.getString("nome"); 
+            String email = rs.getString("email");
+            String telefone = rs.getString("telefone"); 
+
+            System.out.println("Nome: "+nome+" | Email: "+email+" | Telefone: "+telefone);
+
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        }
+
     }
 }
