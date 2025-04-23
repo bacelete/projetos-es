4cc8b9a3f58d6ac3f23f8b2a48907b738a4cd70e
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-22 22:20:35 -0300

    refatoracao de codigo

--- projeto-agenda-java/src/dao/ContatoDAO.java
+++ projeto-agenda-java/src/dao/ContatoDAO.java
@@ -4,19 +4,19 @@ import java.sql.*;
 import database.Conexao; //importa a classe do database
 
 public class ContatoDAO {
-    Connection con = Conexao.connect(); 
+    static Connection con = Conexao.connect(); 
 
-    public void buscarContato(String nome) {
+    public static void buscarContato(String nome) {
         String sql = "SELECT * WHERE nome = ?"; 
         try {
             PreparedStatement query = con.prepareStatement(sql);
             ResultSet rs = query.executeQuery(sql);
 
-            String nome = rs.getString("nome"); 
+            String name = rs.getString("nome"); 
             String email = rs.getString("email");
             String telefone = rs.getString("telefone"); 
 
-            System.out.println("Nome: "+nome+" | Email: "+email+" | Telefone: "+telefone);
+            System.out.println("Nome: "+name+" | Email: "+email+" | Telefone: "+telefone);
 
         } catch (SQLException e) {
             e.printStackTrace(); 
