d666bb745fa709aa3484e4f987b5b14cbaab4bc0
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-23 09:14:07 -0300

    exclusao de imports desnecessarios

--- projeto-agenda-java/src/dao/ContatoDAO.java
+++ projeto-agenda-java/src/dao/ContatoDAO.java
@@ -1,8 +1,5 @@
 package dao;
 
-import java.sql.*; 
-import database.Conexao; //importa a classe do database
-
 public class ContatoDAO extends GenericDAO {
     public void buscarContato(String nome) {
         String sql = "SELECT * WHERE nome = ?"; 
