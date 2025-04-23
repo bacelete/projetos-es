348751807de007a95eba80026eaee0423b36e6ae
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-22 22:10:44 -0300

    criacao da classe ContatoDAO.java

---
+++ projeto-agenda-java/src/dao/ContatoDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import java.sql.*; 
+
+public class ContatoDAO {
+    public void buscarContato(String nome) {
+        String sql = "SELECT * WHERE nome = ?"; 
+
+        
+    }
+}
