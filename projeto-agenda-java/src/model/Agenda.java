9f47466ed783536e48c06444b0f17330c26b07c5
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-23 10:07:17 -0300

    refatoracao de codigo, estruturando a camada AgendaService.java

--- projeto-agenda-java/src/model/Agenda.java
+++ projeto-agenda-java/src/model/Agenda.java
@@ -6,7 +6,6 @@ import model.WriterAndReader;
 
 public class Agenda {
     private ArrayList<Contato> contatos;
-    private static final String PATH = "agenda.txt"; 
 
     public Agenda() {
         try {
@@ -17,69 +16,8 @@ public class Agenda {
         }
     }
 
-    public void adicionarContato(Contato contato) {
-        contatos.add(contato); 
-        try {
-            WriterAndReader.escrever(contato, PATH); 
-        }
-        catch (IOException e) {
-            e.getStackTrace(); 
-        }
-    }
-
-    public void listarContatos() {
-        if (contatos.size() <= 0) {
-            throw new ArithmeticException("A lista de contatos esta vazia"); 
-        }
-
-        System.out.println();
-        for (Contato contato : contatos) {
-            System.out.println(
-                "Nome: "+contato.getNome()+" | Telefone: "+contato.getTelefone()+ " | E-mail: "+contato.getEmail()
-            );
-        }
-    }
-
-    public Contato buscarContato(String nome) { 
-        Contato contatoEncontrado = null; 
-
-        if (nome.isEmpty() || nome.isBlank()) {
-            throw new IllegalArgumentException("Nome deve ser valido"); 
-        }
-
-        if (contatos.size() <= 0) {
-            throw new ArithmeticException("A lista de contatos esta vazia"); 
-        }
-
-        for (Contato contato : contatos) {
-            if (contato.getNome().equalsIgnoreCase(nome)) {
-                contatoEncontrado = contato; 
-            }
-        }
-
-        exibirInfoContato(contatoEncontrado);
-        return contatoEncontrado; 
-    }
-
-    public void removerContato(String nome) {
-        if (contatos.size() <= 0) {
-            throw new ArithmeticException("A lista de contatos esta vazia"); 
-        }
-
-        for (Contato contato : contatos) {
-            if (contato.getNome().toLowerCase() == nome.toLowerCase()) {
-                contatos.remove(contato); 
-                
-            }
-        }
-
-    }
-
-    private void exibirInfoContato(Contato contato) {
-        System.out.println("\nContato encontrado:");
-        System.out.println(
-                "Nome: "+contato.getNome()+" | Telefone: "+contato.getTelefone()+ " | E-mail: "+contato.getEmail()
-        );
+    public ArrayList<Contato> getContatos() {
+        return contatos; 
     }
 
 }
--- projeto-agenda-java/src/service/AgendaService.java
+++ projeto-agenda-java/src/service/AgendaService.java
@@ -1,7 +1,78 @@
 package service;
 
+import java.io.IOException;
+
 import dao.AgendaDAO;
+import model.Contato;
+import model.Contato;
+import model.WriterAndReader;
 
 public class AgendaService {
-    AgendaDAO agenda; 
+    private AgendaDAO agendaDAO; 
+    private Agenda agenda; 
+    private static final String PATH = "agenda.txt"; 
+
+    public void adicionarContato(Contato contato) {
+        try {
+            WriterAndReader.escrever(contato, PATH); 
+        }
+        catch (IOException e) {
+            e.getStackTrace(); 
+        }
+    }
+
+    public void listarContatos() {
+        if (contatos.size() <= 0) {
+            throw new ArithmeticException("A lista de contatos esta vazia"); 
+        }
+
+        System.out.println();
+        for (Contato contato : contatos) {
+            System.out.println(
+                "Nome: "+contato.getNome()+" | Telefone: "+contato.getTelefone()+ " | E-mail: "+contato.getEmail()
+            );
+        }
+    }
+
+    public Contato buscarContato(String nome) { 
+        Contato contatoEncontrado = null; 
+
+        if (nome.isEmpty() || nome.isBlank()) {
+            throw new IllegalArgumentException("Nome deve ser valido"); 
+        }
+
+        if (contatos.size() <= 0) {
+            throw new ArithmeticException("A lista de contatos esta vazia"); 
+        }
+
+        for (Contato contato : contatos) {
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                contatoEncontrado = contato; 
+            }
+        }
+
+        exibirInfoContato(contatoEncontrado);
+        return contatoEncontrado; 
+    }
+
+    public void removerContato(String nome) {
+        if (contatos.size() <= 0) {
+            throw new ArithmeticException("A lista de contatos esta vazia"); 
+        }
+
+        for (Contato contato : contatos) {
+            if (contato.getNome().toLowerCase() == nome.toLowerCase()) {
+                contatos.remove(contato); 
+                
+            }
+        }
+
+    }
+
+    private void exibirInfoContato(Contato contato) {
+        System.out.println("\nContato encontrado:");
+        System.out.println(
+                "Nome: "+contato.getNome()+" | Telefone: "+contato.getTelefone()+ " | E-mail: "+contato.getEmail()
+        );
+    }
 }
