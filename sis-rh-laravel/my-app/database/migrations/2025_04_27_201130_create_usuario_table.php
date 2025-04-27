860b52f18fc615a968cdd0e0ad72f50433fe79bf
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-27 17:23:16 -0300

    colunas iniciais da tabela usuario

--- sis-rh-laravel/my-app/database/migrations/2025_04_27_201130_create_usuario_table.php
+++ sis-rh-laravel/my-app/database/migrations/2025_04_27_201130_create_usuario_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('usuario', function (Blueprint $table) {
             $table->id();
+            $table->string("nome");
+            $table->string("senha");
+            $table->enum("tipo", ["GESTOR", "RH"]); 
             $table->timestamps();
         });
     }
