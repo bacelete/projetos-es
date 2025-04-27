f81ff924f5e0c34a66556fbae3bec19e666dd2a5
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-27 19:56:25 -0300

    seeder da tabela servidor

---
+++ sis-rh-laravel/my-app/database/seeders/ServidorSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+
+class ServidorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('servidor')->insert([
+            'nome' => Str::random(20)
+        ]);
+    }
+}
