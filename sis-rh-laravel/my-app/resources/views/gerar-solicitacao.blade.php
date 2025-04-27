52d96caa60a61eb9b408e35c0d26847b01bf6858
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-26 23:54:15 -0300

    alteracao do href do botao voltar do gerar-solicitacao.blade.php

--- sis-rh-laravel/my-app/resources/views/gerar-solicitacao.blade.php
+++ sis-rh-laravel/my-app/resources/views/gerar-solicitacao.blade.php
@@ -76,7 +76,7 @@
                 </div>
 
                 <div class="d-flex mt-4 justify-content-between">
-                    <a href="#" name="voltar" id="voltar" class="btn w-20 justify-content-center text-white mt-4 bg-danger">Voltar</a>
+                    <a href="/solicitacoes" name="voltar" id="voltar" class="btn w-20 justify-content-center text-white mt-4 bg-danger">Voltar</a>
                     <button type="submit" class="btn d-block w-20 justify-content-center text-white mt-4 bg-secondary" id="enviarSolicitacao" name="enviarSolicitacao">Enviar</button>
                 </div>
 
