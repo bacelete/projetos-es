d54779768e4f96f8e91d0a2862b248876ca4056d
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-27 20:31:27 -0300

    refatoracao dos seeders

--- sis-rh-laravel/my-app/database/seeders/UsuarioSeeder.php
+++ sis-rh-laravel/my-app/database/seeders/UsuarioSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB; 
 use Illuminate\Support\Facades\Hash;
@@ -16,7 +15,7 @@ class UsuarioSeeder extends Seeder
     public function run(): void
     {
         DB::table('usuario')->insert([
-            'usuario' => Str::random(10), 
+            'nome' => Str::random(10), 
             'senha' => Hash::make('password'),
             'tipo' => $this->getTipos()
         ]);
