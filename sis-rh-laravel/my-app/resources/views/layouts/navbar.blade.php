aa03603ddcd134231e7e5519a07a53df48412884
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-26 23:29:21 -0300

    algumas mudanças no html

--- sis-rh-laravel/my-app/resources/views/layouts/navbar.blade.php
+++ sis-rh-laravel/my-app/resources/views/layouts/navbar.blade.php
@@ -18,10 +18,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-2">
                     <li class="nav-item">
-                        <a class="nav-link active text-light" aria-current="page" href="../components/home.php">Home</a>
+                        <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-light" href="listar-solicitacao.php"></i>Listar</a>
+                        <a class="nav-link text-light" href="/solicitacoes"></i>Listar</a>
                     </li>
                 </ul>
             </div>
