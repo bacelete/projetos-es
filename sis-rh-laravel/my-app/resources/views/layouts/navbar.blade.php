9146b5db02deba91aaa385647f85b2f6ed4c9dcb
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-26 22:36:48 -0300

    componente da navbar pronta, so dar um extend

---
+++ sis-rh-laravel/my-app/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid mt-2">
+            <a style="font-size: 2.5rem" class="navbar-brand text-light align-self-center fw-bold" href="#">SIRH</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-2">
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page" href="../components/home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="listar-solicitacao.php"></i>Listar</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <button id="recolherMenu" style="display: none" name="recolherMenu" class="btn btn-sm btn-outline-light mt-auto m-4 align-self-center"><i class="fa-solid fa-arrow-left m-1"></i>Recolher menu</button>
+    </nav>
+
+    <script src="https://kit.fontawesome.com/291cf6cb9c.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
