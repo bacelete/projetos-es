84e7675dbb2948e47cee82cbee71cbde1751cb68
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-27 20:19:21 -0300

    seeder do usuario + funcao random do tipo

--- sis-rh-laravel/my-app/database/seeders/UsuarioSeeder.php
+++ sis-rh-laravel/my-app/database/seeders/UsuarioSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsuarioSeeder extends Seeder
 {
@@ -12,6 +15,17 @@ class UsuarioSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('usuario')->insert([
+            'usuario' => Str::random(10), 
+            'senha' => Hash::make('password'),
+            'tipo' => $this->getTipos()
+        ]);
     }
+
+    public function getTipos(): string 
+    {
+        $tipos = ["RH", "GESTOR"];
+        return $tipos[array_rand($tipos)]; 
+    }
+
 }
