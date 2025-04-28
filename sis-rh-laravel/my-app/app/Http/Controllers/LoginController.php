06c1fa449abb2f7d0403e1d84b15f0a47bd56f8b
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-28 10:48:07 -0300

    logica de login do LoginController

--- sis-rh-laravel/my-app/app/Http/Controllers/LoginController.php
+++ sis-rh-laravel/my-app/app/Http/Controllers/LoginController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return Redirect::intended('dashboard');
+        }
+    }
 }
