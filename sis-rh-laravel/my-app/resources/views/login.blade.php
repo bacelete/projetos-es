49ed6c22b9ef8104db9c0e3a7bbfebb67e9ac27e
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-28 10:33:23 -0300

    tela de login

---
+++ sis-rh-laravel/my-app/resources/views/login.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.navbar')
+
+@section('title', 'Login')
+@section('content')
+
+<div class="login-box">
+        <h1 id="titulo-login">Login</h1>
+        <form action="../backend/validar-login.php" method="post">
+            <div class="box-input">
+                <i class="fa-solid fa-envelope"></i>
+                <input type="text" name="usuario" placeholder="Digite seu usuário">
+            </div>
+            <div class="box-input">
+                <i class="fa-solid fa-lock"></i>
+                <input type="password" name="senha" placeholder="Digite sua senha">
+            </div>
+            <button class="login-button" type="submit">Entrar</button>
+        </form>
+</div>
+@endsection
