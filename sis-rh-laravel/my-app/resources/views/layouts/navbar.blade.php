ddd466b03e32f02f1b4f22841d0b536b1eec58f2
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-29 19:48:35 -0300

    correção da pagina mal formatada

--- sis-rh-laravel/my-app/resources/views/layouts/navbar.blade.php
+++ sis-rh-laravel/my-app/resources/views/layouts/navbar.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title> <!-- Título dinâmico -->
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
