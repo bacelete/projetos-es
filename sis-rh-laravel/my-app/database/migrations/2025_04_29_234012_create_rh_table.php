b75cd62ff5ef4b0fad1f487b4e7855b5c8acb111
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-29 20:44:29 -0300

    schema criado pro rh

--- sis-rh-laravel/my-app/database/migrations/2025_04_29_234012_create_rh_table.php
+++ sis-rh-laravel/my-app/database/migrations/2025_04_29_234012_create_rh_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('rh', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('password');
         });
     }
 
