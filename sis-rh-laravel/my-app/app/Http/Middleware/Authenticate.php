68c9729a0e0f25eeebe12c1db599f0a716309c23
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-30 09:05:03 -0300

    copie e colei a funcao de redirecionamento

--- sis-rh-laravel/my-app/app/Http/Middleware/Authenticate.php
+++ sis-rh-laravel/my-app/app/Http/Middleware/Authenticate.php
@@ -4,18 +4,44 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Arr;
 
-class Authenticate
+class Authenticate extends Middleware
 {
+    protected $guards;
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string[]  ...$guards
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next, ...$guards)
     {
-        return $next($request);
+        $this->guards = $guards;
+
+        return parent::handle($request, $next, ...$guards);
+    }
+
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            if (Arr::first($this->guards) === 'admin') {
+                return route('admin.login');
+            } else if(Arr::first($this->guards) === 'vendor') {
+                return route('vendor.login');
+            }
+        }
     }
 }
