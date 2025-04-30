9a003196227cbf02a23d509ad9245c5d0de67dca
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-04-30 09:09:29 -0300

    comentarios de rotas

--- sis-rh-laravel/my-app/routes/web.php
+++ sis-rh-laravel/my-app/routes/web.php
@@ -9,19 +9,21 @@ Route::get('/', function () {
     echo "Home";
 })->name('home');
 
-//rotas da solicitação
+//rotas do rh:
 Route::middleware(('auth:rh'), function () {
     Route::get('/solicitacoes', [RhController::class, 'index']);
 });
+
+//rotas do gestor:
 Route::middleware(('auth:gestor'), function () {
     Route::get('/solicitacao', [GestorController::class, 'index']);
     Route::post('/solicitacao/store', [GestorController::class, 'store']);
 });
 
 //rota de autenticação: 
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/login', [LoginController::class, 'show']);
-Route::post('/login', [LoginController::class, 'login'])->name('login');
+//Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+//Route::get('/login', [LoginController::class, 'show']);
+//Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 require __DIR__ . '/settings.php';
 // require __DIR__.'/auth.php';
