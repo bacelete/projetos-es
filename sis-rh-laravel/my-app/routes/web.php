cd220463cede81280b5cff1f722217df299b124a
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-05-03 20:05:34 -0300

    rota de mostrar o form pra editar

--- sis-rh-laravel/my-app/routes/web.php
+++ sis-rh-laravel/my-app/routes/web.php
@@ -24,6 +24,9 @@ Route::post('/solicitacao/store', [GestorController::class, 'store'])
 Route::post('/solicitacao/delete/{id}', [GestorController::class, 'delete'])
     ->middleware('auth:gestor')
     ->name('excluir-solicitacao');
+Route::get('/solicitacao/edit', [GestorController::class, 'show'])
+    ->middleware('auth:gestor')
+    ->name('editar-solicitacao');
 Route::put('/solicitacao/edit/{id}', [GestorController::class, 'edit'])
     ->middleware('auth:gestor')
     ->name('editar-solicitacao');
