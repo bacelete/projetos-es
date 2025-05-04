a903875c0dbef42d1a92b515ca59d3642654d608
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-05-03 22:06:13 -0300

    funcao index funcional no solicitacao controller

--- sis-rh-laravel/my-app/app/Http/Controllers/SolicitacaoController.php
+++ sis-rh-laravel/my-app/app/Http/Controllers/SolicitacaoController.php
@@ -8,13 +8,16 @@ use App\Models\Solicitacao;
 class SolicitacaoController extends Controller
 {
     public function index() 
-    {
-        if (Auth::guard('rh')) {
-            $solicitacoes = Solicitacao::all(); 
-        }
-        if (Auth::guard('gestor')) {
-            $solicitacoes = Solicitacao::where('id_gestor', auth()->guard()->id())->get(); 
-        }
-        return view('listar-solicitacao', compact('solicitacoes')); 
+{
+    if (Auth::guard('rh')->check()) {
+        $solicitacoes = Solicitacao::all(); 
+    } elseif (Auth::guard('gestor')->check()) {
+        $user = Auth::guard('gestor')->user();
+        $solicitacoes = Solicitacao::where('id_gestor', $user->id)->get(); 
+    } else {
+        abort(403, 'Acesso não autorizado.');
     }
+
+    return view('listar-solicitacao', compact('solicitacoes')); 
+}
 }
