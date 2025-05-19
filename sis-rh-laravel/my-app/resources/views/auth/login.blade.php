0b89dcb3b2567f363094e4248c674c9c23133f82
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-05-19 11:32:47 -0300

    + boostrap na tela de login

--- sis-rh-laravel/my-app/resources/views/auth/login.blade.php
+++ sis-rh-laravel/my-app/resources/views/auth/login.blade.php
@@ -9,19 +9,23 @@
 </head>
 
 <body>
-    <div class="card border-light mb-3 w-50">
-        <div class="card-header"><h2>Login</h2></div>
+    <div class="card border-light mx-auto w-50">
+        <div class="card-header">
+            <h2>Login</h2>
+        </div>
         <div class="card-body">
             <div class="form-floating mb-3">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+                <label for="floatingInput">Endereço de email</label>
             </div>
             <div class="form-floating">
                 <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
+                <label for="floatingPassword">Senha</label>
             </div>
         </div>
-        
+        <form action="/login" method="post"></form>
+        <button class="btn btn-dark">Entrar</button>
+    </div>
     </div>
     <script src="https://kit.fontawesome.com/291cf6cb9c.js" crossorigin="anonymous"></script>
 </body>
