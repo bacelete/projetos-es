421b0d9d0a7bada640016b96ea2bd46452db7ec8
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-05-22 13:26:08 -0300

    add icone de lapis no editar

--- sis-rh-laravel/my-app/resources/views/editar-solicitacao.blade.php
+++ sis-rh-laravel/my-app/resources/views/editar-solicitacao.blade.php
@@ -8,7 +8,7 @@
 <div class="container w-75">
             <div id="liveAlertPlaceholder"></div>
             <div class="shadow mt-5">
-                <div class="card-header h2 p-4 bg-dark text-white rounded-2">Editar solicitação</div>
+                <div class="card-header h2 p-4 bg-dark text-white rounded-2"><i class="fa-solid fa-pen-to-square m-2"></i>Editar solicitação</div>
                 <div class="card-body mt-2 p-4">
                     <form method="POST" action="/solicitacao/edit/{{ $solicitacao->id }}/save" class="needs-validation" novalidate>
                         @csrf
