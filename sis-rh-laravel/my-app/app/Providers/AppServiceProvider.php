f5d9dde6b61a88da8b9faa2925902cfb0e495f05
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-05-30 10:56:54 -0300

    logica inicial do gate facade para editar solicitacao

--- sis-rh-laravel/my-app/app/Providers/AppServiceProvider.php
+++ sis-rh-laravel/my-app/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Solicitacao;
+use App\Models\Gestor;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('update-solicitacao', function (Gestor $user, Solicitacao $solicitacao) {
+            return $user->id === $solicitacao->user_id;
+        });
     }
 }
