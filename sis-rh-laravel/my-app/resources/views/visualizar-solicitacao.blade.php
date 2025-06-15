8df77ab1f8bcfaa9be3a4be3cdeb86131bd66e30
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-06-15 19:58:45 -0300

    refatoracao da view

--- sis-rh-laravel/my-app/resources/views/visualizar-solicitacao.blade.php
+++ sis-rh-laravel/my-app/resources/views/visualizar-solicitacao.blade.php
@@ -8,22 +8,22 @@
         <h5 class="card-title">Solicitação</h5>
         <h6 class="card-subtitle mb-2 text-body-secondary">Visualize as informações da solicitação.</h6>
         <div class="d-inline-flex p-2">
-            <div class="d-flex m-2">
+            <div class="d-flex align-items-center m-2">
                 <span class="h6 mx-2">Nome: </span>
                 <div class="card">
-                    <span>{{$solicitacao->servidor->name}}</span>
+                    <span class="mx-4">{{$solicitacao->servidor->name}}</span>
                 </div>
             </div>
             <div class="d-flex m-2">
                 <span class="h6 mx-2">CPF: </span>
                 <div class="card">
-                    <span>{{$solicitacao->servidor->cpf}}</span>
+                    <span class="mx-4">{{$solicitacao->servidor->cpf}}</span>
                 </div>
             </div>
             <div class="d-flex m-2">
                 <span class="h6 mx-2">Unidade: </span>
                 <div class="card">
-                    <span>{{$solicitacao->unidade}}</span>
+                    <span class="mx-4">{{$solicitacao->unidade}}</span>
                 </div>
             </div>
         </div>
