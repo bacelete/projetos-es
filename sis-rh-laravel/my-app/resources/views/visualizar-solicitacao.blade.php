8cc82d8f27d56d56b583b47ba49e8a010ccc276e
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-06-15 20:03:55 -0300

    refatoracao de flex para grid

--- sis-rh-laravel/my-app/resources/views/visualizar-solicitacao.blade.php
+++ sis-rh-laravel/my-app/resources/views/visualizar-solicitacao.blade.php
@@ -4,28 +4,39 @@
 @section('content')
 
 <div class="card" style="width: 80rem;">
-    <div class="card-body">
+    <div class="card-body p-4">
         <h5 class="card-title">Solicitação</h5>
         <h6 class="card-subtitle mb-2 text-body-secondary">Visualize as informações da solicitação.</h6>
-        <div class="d-inline-flex p-2">
-            <div class="d-flex align-items-center m-2">
-                <span class="h6 mx-2">Nome: </span>
-                <div class="card">
-                    <span class="mx-4">{{$solicitacao->servidor->name}}</span>
+        <div class="row p-2">
+            <div class="col">
+                <div class="align-items-center m-2">
+                    <span class="h6 mx-2">Nome: </span>
+                    <div class="card">
+                        <span class="mx-4">{{$solicitacao->servidor->name}}</span>
+                    </div>
+                </div>
+                <div class="m-2">
+                    <span class="h6 mx-2">CPF: </span>
+                    <div class="card">
+                        <span class="mx-4">{{$solicitacao->servidor->cpf}}</span>
+                    </div>
                 </div>
             </div>
-            <div class="d-flex m-2">
-                <span class="h6 mx-2">CPF: </span>
-                <div class="card">
-                    <span class="mx-4">{{$solicitacao->servidor->cpf}}</span>
+            <div class="col">
+                <div class="m-2">
+                    <span class="h6 mx-2">Unidade: </span>
+                    <div class="card">
+                        <span class="mx-4">{{$solicitacao->unidade}}</span>
+                    </div>
+                </div>
+                <div class="m-2">
+                    <span class="h6 mx-2">Motivo: </span>
+                    <div class="card">
+                        <span class="mx-4">{{$solicitacao->motivo}}</span>
+                    </div>
                 </div>
             </div>
-            <div class="d-flex m-2">
-                <span class="h6 mx-2">Unidade: </span>
-                <div class="card">
-                    <span class="mx-4">{{$solicitacao->unidade}}</span>
-                </div>
-            </div>
+
         </div>
     </div>
 </div>
