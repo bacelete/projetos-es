ea38c652fb501d20293a3785f4c26a62677ad7e2
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-06-15 19:38:10 -0300

    Update visualizar-solicitacao.blade.php

--- sis-rh-laravel/my-app/resources/views/visualizar-solicitacao.blade.php
+++ sis-rh-laravel/my-app/resources/views/visualizar-solicitacao.blade.php
@@ -3,6 +3,30 @@
 @section('title', 'Visualizar Solicitação')
 @section('content')
 
-<h1>Oi!</h1>
-
+<div class="card" style="width: 80rem;">
+    <div class="card-body">
+        <h5 class="card-title">Solicitação</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">Visualize as informações da solicitação.</h6>
+        <div class="d-inline-flex p-2">
+            <div class="d-flex m-2">
+                <span class="h6 mx-2">Nome: </span>
+                <div class="card">
+                    <span>{{$solicitacao->servidor->name}}</span>
+                </div>
+            </div>
+            <div class="d-flex m-2">
+                <span class="h6 mx-2">CPF: </span>
+                <div class="card">
+                    <span>{{$solicitacao->servidor->cpf}}</span>
+                </div>
+            </div>
+            <div class="d-flex m-2">
+                <span class="h6 mx-2">Unidade: </span>
+                <div class="card">
+                    <span>{{$solicitacao->unidade}}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
