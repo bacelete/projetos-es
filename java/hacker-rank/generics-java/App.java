948ef9c645956cd9b5177f48998b80d649383f86
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-07-10 12:13:08 -0300

    refatoracao de codigo

--- java/hacker-rank/generics-java/App.java
+++ java/hacker-rank/generics-java/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class App<T>{
     private static final Scanner scanner = new Scanner(System.in);
 
-    public void printArray(T[] arr)  {
+    public static <T> void printArray(T[] arr)  {
         for (T o : arr) {
             System.out.println(o);
         }
@@ -14,11 +14,7 @@ public class App<T>{
         Integer[] arr = {1, 2, 3};
         String[] strings = {"Hello", "World"};
 
-        App<Integer> app1 = new App<Integer>();
-        app1.printArray(arr);
-
-        App<String> app2 = new App<String>();
-        app2.printArray(strings);
-
+        printArray(arr);
+        printArray(strings);
     }
 }
